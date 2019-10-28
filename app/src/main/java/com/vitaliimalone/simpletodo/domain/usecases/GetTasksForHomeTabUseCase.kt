d603ebc45fa284457e5bafbaa1393241cf975ae6
Vitalii Malyi <vitaliimalone@gmail.com>
2019-10-29 00:52:37 +0200

    Add correct tasks query for week tab

--- app/src/main/java/com/vitaliimalone/simpletodo/domain/usecases/GetTasksForHomeTabUseCase.kt
+++ app/src/main/java/com/vitaliimalone/simpletodo/domain/usecases/GetTasksForHomeTabUseCase.kt
@@ -4,17 +4,21 @@ import com.vitaliimalone.simpletodo.data.repository.TaskRepository
 import com.vitaliimalone.simpletodo.domain.mappers.TaskMapper
 import com.vitaliimalone.simpletodo.domain.models.Task
 import com.vitaliimalone.simpletodo.presentation.models.HomeTab
+import com.vitaliimalone.simpletodo.presentation.utils.Constants
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import org.threeten.bp.DayOfWeek
+import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.OffsetTime
 import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.temporal.TemporalAdjusters
 
 class GetTasksForHomeTabUseCase(
         private val taskRepository: TaskRepository
 ) {
-    suspend fun getTasksForTab(tab: HomeTab): Flow<List<Task>> { // todo fix
+    suspend fun getTasksForTab(tab: HomeTab): Flow<List<Task>> {
         val startDate: String
         val endDate: String
         when (tab) {
@@ -27,32 +31,61 @@ class GetTasksForHomeTabUseCase(
                         .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
             }
             HomeTab.WEEK -> {
-                startDate = OffsetDateTime.now()
-                        .plusDays(1)
-                        .with(LocalTime.MIN)
-                        .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                endDate = OffsetDateTime.now()
-                        .plusDays(6) // todo fix
-                        .with(LocalTime.MAX)
-                        .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                val todayDayOfWeek = LocalDate.now().dayOfWeek
+                if (todayDayOfWeek < DayOfWeek.SUNDAY) {
+                    startDate = OffsetDateTime.now()
+                            .plusDays(1)
+                            .with(LocalTime.MIN)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    endDate = OffsetDateTime.now()
+                            .with(DayOfWeek.SUNDAY)
+                            .with(LocalTime.MAX)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                } else {
+                    // next week
+                    startDate = OffsetDateTime.now()
+                            .plusWeeks(1)
+                            .with(DayOfWeek.MONDAY)
+                            .with(LocalTime.MIN)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    endDate = OffsetDateTime.now()
+                            .plusWeeks(1)
+                            .with(DayOfWeek.SUNDAY)
+                            .with(LocalTime.MIN)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                }
             }
             HomeTab.MONTH -> {
-                startDate = OffsetDateTime.now()
-                        .plusDays(7) // todo fix
-                        .with(LocalTime.MIN)
-                        .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                endDate = OffsetDateTime.now()
-                        .plusDays(23) // todo fix
-                        .with(LocalTime.MAX)
-                        .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                if (true) { // todo fix
+                    startDate = OffsetDateTime.now()
+                            .plusDays(7) // todo fix
+                            .with(LocalTime.MIN)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    endDate = OffsetDateTime.now()
+                            .plusDays(23) // todo fix
+                            .with(LocalTime.MAX)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                } else {
+                    // next month
+                    startDate = OffsetDateTime.now()
+                            .plusMonths(1)
+                            .with(TemporalAdjusters.firstDayOfMonth())
+                            .with(LocalTime.MIN)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                    endDate = OffsetDateTime.now()
+                            .plusMonths(1)
+                            .with(TemporalAdjusters.lastDayOfMonth())
+                            .with(LocalTime.MIN)
+                            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                }
             }
-            HomeTab.TODO -> {
+            HomeTab.TODO -> { // todo fix
                 startDate = OffsetDateTime.now()
                         .plusDays(23) // todo fix
                         .with(LocalTime.MIN)
                         .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
                 endDate = OffsetDateTime.now()
-                        .withYear(9999) // todo ?
+                        .withYear(Constants.MAX_YEAR)
                         .with(OffsetTime.MAX)
                         .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
             }
---
+++ app/src/main/java/com/vitaliimalone/simpletodo/presentation/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.vitaliimalone.simpletodo.presentation.utils
+
+object Constants {
+    const val MAX_YEAR = 9999
+}
