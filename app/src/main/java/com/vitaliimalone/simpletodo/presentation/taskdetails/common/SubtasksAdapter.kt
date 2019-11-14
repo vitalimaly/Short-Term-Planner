355f5782bf56e0523ab79a854c87529d934ffc99
Vitalii Malyi <vitaliimalone@gmail.com>
2019-11-14 18:54:20 +0200

    Add subtask adapter types

--- app/src/main/java/com/vitaliimalone/simpletodo/presentation/taskdetails/common/SubtasksAdapter.kt
+++ app/src/main/java/com/vitaliimalone/simpletodo/presentation/taskdetails/common/SubtasksAdapter.kt
@@ -7,27 +7,49 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.vitaliimalone.simpletodo.R
 import com.vitaliimalone.simpletodo.domain.models.Subtask
+import com.vitaliimalone.simpletodo.presentation.utils.Res
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.task_details_subtasks_list_item.*
 
-class SubtasksAdapter : RecyclerView.Adapter<SubtasksAdapter.TaskViewHolder>() {
-    var subtasks = listOf<Subtask>()
+class SubtasksAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private enum class SubtaskViewType { SUBTASK, ADDNEW }
+
+    var subtasks = mutableListOf<Subtask>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.task_details_subtasks_list_item, parent, false)
-        return TaskViewHolder(view)
+        return when (viewType) {
+            SubtaskViewType.SUBTASK.ordinal -> TaskViewHolder(view)
+            SubtaskViewType.ADDNEW.ordinal -> AddNewTaskViewHolder(view)
+            else -> throw IllegalArgumentException()
+        }
     }
 
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bind(subtasks[position], position)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is TaskViewHolder -> holder.bind(subtasks[position], position)
+            is AddNewTaskViewHolder -> holder.bind()
+            else -> throw IllegalArgumentException()
+        }
     }
 
     override fun getItemCount(): Int {
-        return subtasks.size
+        return if (subtasks.isEmpty()) {
+            1
+        } else {
+            subtasks.size + 1
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            subtasks.size -> SubtaskViewType.ADDNEW.ordinal
+            else -> SubtaskViewType.SUBTASK.ordinal
+        }
     }
 
     inner class TaskViewHolder(
@@ -36,7 +58,31 @@ class SubtasksAdapter : RecyclerView.Adapter<SubtasksAdapter.TaskViewHolder>() {
         fun bind(subtask: Subtask, position: Int) {
             taskTitleEt.setText(subtask.title)
             doneCheckBox.isChecked = subtask.isDone
+
+            topLineView.isVisible = true
+            horizontalLineView.isVisible = true
             botLineView.isVisible = subtasks.lastIndex != position
+        }
+    }
+
+    inner class AddNewTaskViewHolder(
+            override val containerView: View
+    ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind() {
+            doneCheckBox.buttonDrawable = Res.drawable(R.drawable.ic_add_checkbox)
+            taskTitleEt.text?.clear()
+            taskTitleEt.isFocusable = false
+            taskTitleEt.isFocusableInTouchMode = false
+            doneCheckBox.isChecked = false
+
+            topLineView.isVisible = false
+            horizontalLineView.isVisible = false
+            botLineView.isVisible = false
+
+            taskTitleEt.setOnClickListener {
+                subtasks.add(Subtask())
+                notifyDataSetChanged()
+            }
         }
     }
 }
