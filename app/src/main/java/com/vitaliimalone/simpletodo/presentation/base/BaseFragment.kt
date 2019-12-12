db967a908c958da54287f05ef91f63ecb1bf078d
Vitalii Malyi <vitaliimalone@gmail.com>
2019-12-12 13:23:32 +0200

    Update base Fragment

--- app/src/main/java/com/vitaliimalone/simpletodo/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/vitaliimalone/simpletodo/presentation/base/BaseFragment.kt
@@ -1,17 +1,10 @@
 package com.vitaliimalone.simpletodo.presentation.base
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.vitaliimalone.simpletodo.presentation.utils.hideKeyboard
 
-abstract class BaseFragment(private val layoutResId: Int) : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return LayoutInflater.from(activity).inflate(layoutResId, container, false)
-    }
-
+abstract class BaseFragment(@LayoutRes layoutResId: Int) : Fragment(layoutResId) {
     override fun onStop() {
         super.onStop()
         hideKeyboard()
